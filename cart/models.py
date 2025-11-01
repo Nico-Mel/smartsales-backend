94d664b738afbf6af6d10f70f3500d34e24e3008
NohemiChoqueED2 <universidad.nohemi@gmail.com>
2025-10-31 21:28:32 -0400

    feat(cart): add models for Cart and CartItem

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+class Cart(models.Model):
+    usuario = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now=True)
+    estado = models.CharField(max_length=20, choices=[
+        ('activo', 'Activo'),
+        ('confirmado', 'Confirmado'),
+        ('cancelado', 'Cancelado'),
+        ('inactivo', 'Inactivo'),
+    ], default='activo')
+
+    class Meta:
+        db_table = 'cart'
+    
+    def __str__(self):
+        return f"Carrito #{self.id} - {self.usuario.email} - {self.estado}"
+    
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, related_name = 'item',on_delete=models.CASCADE)
+    producto = models.ForeignKey('products.Producto', on_delete=models.CASCADE)
+    cantidad = models.PositiveIntegerField()
+    precio_unitario = models.DecimalField(max_digits=10, decimal_places=2)
+    class Meta:
+        db_table = 'cart_item'
+    def __str__(self):
+        return f"Item #{self.id} - {self.producto.nombre} - {self.cantidad}"
